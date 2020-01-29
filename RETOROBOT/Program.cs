142012467ffbeaacd3c2dd6dff682d30cfae66e9
FELIPE GIRALDO <felipe.giraldo@endava.com>
2020-01-29 11:56:59 -0500

    Erasing DIjkstra from source

--- RETOROBOT/Program.cs
+++ RETOROBOT/Program.cs
@@ -13,7 +13,7 @@ namespace ROBOT_APP
 
     public static class Program
     {
-        /*
+        
 
         static void Main(string[] args)
         {
@@ -202,78 +202,8 @@ namespace ROBOT_APP
                 Console.WriteLine(Environment.NewLine);
             }
 
-
         }
     
-     */
-
-        private static int MinimumDistance(int[] distance, bool[] shortestPathTreeSet, int verticesCount)
-        {
-            int min = int.MaxValue;
-            int minIndex = 0;
-
-            for (int v = 0; v < verticesCount; ++v)
-            {
-                if (shortestPathTreeSet[v] == false && distance[v] <= min)
-                {
-                    min = distance[v];
-                    minIndex = v;
-                }
-            }
-
-            return minIndex;
-        }
-
-        private static void Print(int[] distance, int verticesCount)
-        {
-            Console.WriteLine("Vertex    Distance from source");
-
-            for (int i = 0; i < verticesCount; ++i)
-                Console.WriteLine("{0}\t  {1}", i, distance[i]);
-        }
-
-        public static void DijkstraAlgo(int[,] graph, int source, int verticesCount)
-        {
-            int[] distance = new int[verticesCount];
-            bool[] shortestPathTreeSet = new bool[verticesCount];
-
-            for (int i = 0; i < verticesCount; ++i)
-            {
-                distance[i] = int.MaxValue;
-                shortestPathTreeSet[i] = false;
-            }
-
-            distance[source] = 0;
-
-            for (int count = 0; count < verticesCount - 1; ++count)
-            {
-                int u = MinimumDistance(distance, shortestPathTreeSet, verticesCount);
-                shortestPathTreeSet[u] = true;
-
-                for (int v = 0; v < verticesCount; ++v)
-                    if (!shortestPathTreeSet[v] && Convert.ToBoolean(graph[u, v]) && distance[u] != int.MaxValue && distance[u] + graph[u, v] < distance[v])
-                        distance[v] = distance[u] + graph[u, v];
-            }
-
-            Print(distance, verticesCount);
-        }
-
-        static void Main(string[] args)
-        {
-            int[,] graph =  {
-                         { 0,2,0,4,7,0,0,0 },
-                         { 2,0,4,0,0,0,0,0},
-                         { 0,4,0,2,0,5,0,0},
-                         { 4,0,2,0,0,0,0,0},
-                         { 7,0,0,0,0,0,0,0},
-                         { 0,0,5,0,0,0,0,5},
-                         { 0,0,0,0,0,0,0,0},
-                         { 0,0,0,0,0,5,0,0}
-                            };
-
-            DijkstraAlgo(graph, 0, 8);
-        }
-
     }
 }
     
